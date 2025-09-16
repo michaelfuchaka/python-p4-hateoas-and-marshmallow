38a18971b1dd8eac5fb69171e78f9ef32cd8579e
michaelfuchaka <fuchakamichael06@gmail.com>
2025-09-16 10:38:34 +0300

    Add Marshmallow integration for Newsletter serialization
    - Introduced NewsletterSchema for serializing Newsletter model instances.
    - Updated Newsletters resource to return serialized data using newsletters_schema.
    - Modified NewsletterByID resource to return serialized data for individual newsletters.
    - Enhanced the create and update methods to use the new schema for response formatting.

--- server/app.py
+++ server/app.py
@@ -15,6 +15,32 @@ app.json.compact = False
 migrate = Migrate(app, db)
 db.init_app(app)
 
+
+ma = Marshmallow(app)
+
+
+class NewsletterSchema(ma.SQLAlchemySchema):
+
+    class Meta:
+        model = Newsletter
+        load_instance = True
+
+    title = ma.auto_field()
+    published_at = ma.auto_field()
+
+
+    url = ma.Hyperlinks(
+        {
+            "self": ma.URLFor(
+                "newsletterbyid",
+                values=dict(id="<id>")),
+            "collection": ma.URLFor("newsletters"),
+        }
+    )
+
+newsletter_schema = NewsletterSchema()
+newsletters_schema = NewsletterSchema(many=True)
+
 api = Api(app)
 
 class Index(Resource):
@@ -38,12 +64,14 @@ class Newsletters(Resource):
 
     def get(self):
         
-        response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
+          newsletters = Newsletter.query.all()
 
-        response = make_response(
-            response_dict_list,
-            200,
-        )
+          response = make_response(
+            newsletters_schema.dump(newsletters),
+        200,
+          )
+  
+        
 
         return response
 
@@ -57,12 +85,10 @@ class Newsletters(Resource):
         db.session.add(new_record)
         db.session.commit()
 
-        response_dict = new_record.to_dict()
-
         response = make_response(
-            response_dict,
-            201,
-        )
+       newsletter_schema.dump(new_record),
+       201, 
+      )
 
         return response
 
@@ -72,14 +98,15 @@ class NewsletterByID(Resource):
 
     def get(self, id):
 
-        response_dict = Newsletter.query.filter_by(id=id).first().to_dict()
+      newsletter = Newsletter.query.filter_by(id=id).first()
 
-        response = make_response(
-            response_dict,
-            200,
-        )
+      response = make_response(
+      newsletter_schema.dump(newsletter),
+     200,
+         )
 
-        return response
+
+      return response
 
     def patch(self, id):
 
@@ -89,13 +116,13 @@ class NewsletterByID(Resource):
 
         db.session.add(record)
         db.session.commit()
-
-        response_dict = record.to_dict()
-
+        
         response = make_response(
-            response_dict,
-            200
-        )
+             newsletter_schema.dump(record),
+           200
+       )
+
+        
 
         return response
 
